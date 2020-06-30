5d2b84f34378e47884d9166eb8297e6ffcebeded
Marc Hofmann <mh@hasomed.de>
2020-06-30 11:21:21 +0200

    Revert "new Version"
    This reverts commit 995e21fdbc44bb8eaaab6529654deaf6d6f39ca1.

--- WebBluetooth/service-worker.js
+++ WebBluetooth/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ac8ba04be1000d110dca6b370e060ed6.js"
+  "./precache-manifest.e6c9d4700591114677ca4efb84552526.js"
 );
 
 self.addEventListener('message', (event) => {
